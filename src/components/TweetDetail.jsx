fda0917d784a876c6ba61f98ee418fde89c6ed36
Matthieu Nzinga <matthieunzinga@gmail.com>
2023-11-22 17:54:02 +0100

    Add index in map

--- src/components/TweetDetail.jsx
+++ src/components/TweetDetail.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function TweetDetail({ tweet }) {
   return (
     <div className="tweets">
-      {tweet.map((element) => (
-        <div className="tweet">
+      {tweet.map((element, index) => (
+        <div key={index} className="tweet">
           <aside className="tweet-avatar">
             <img src={element.imageIcon} alt="" />
           </aside>
           <div className="tweet-content">
             <div className="tweet-body">
-              {element.dataTitles.map((title) => (
-                <h2 className="tweet-title">
+              {element.dataTitles.map((title, id) => (
+                <h2 key={id} className="tweet-title">
                   <span className="tweet-title-author">{title.text} </span>
                   <img src={title.iconCer} alt="" />
                   <span className="tweet-title-details">{title.textMail} </span>
@@ -20,18 +20,17 @@ function TweetDetail({ tweet }) {
               ))}
               <p className="tweet-text">{element.description} </p>
               <div className="tweet-image">
-                <img  src={element.photo} alt="" />
+                <img src={element.photo} alt="" />
               </div>
             </div>
             <div className="tweet-actions">
-                {element.dataLikes.map((like) => (
-                    <div className="tweet-action">
-                      <img src={like.iconLike} alt="" />
-                      <span>{like.count} </span>
-                    </div>
-                ))}
+              {element.dataLikes.map((like, indexId) => (
+                <div key={indexId} className="tweet-action">
+                  <img src={like.iconLike} alt="" />
+                  <span>{like.count} </span>
+                </div>
+              ))}
             </div>
-
           </div>
         </div>
       ))}
